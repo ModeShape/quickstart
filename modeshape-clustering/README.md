3409292abcd4b1589979044cdea9779d85daaf86
Horia Chiorean <hchiorea@redhat.com>
2013-05-09 10:31:55 +0300

    MODE-1695 - Added clustering quickstart.

---
+++ modeshape-clustering/README.md
@@ -0,0 +1,132 @@
+Example Clustering 2 ModeShape Repositories
+===========================================
+
+What is it?
+-----------
+
+This is a self-contained and deployable Maven 3 project that shows how to cluster 2 ModeShape repositories.
+
+System requirements
+-------------------
+
+All you need to build this project is Java 6.0 (Java SDK 1.6) or better, Maven 3.0 or better.
+The application this project produces is designed to be run on JBoss Enterprise Application Platform 6.
+
+Install ModeShape's EAP kit into an existing JBoss EAP 6 server
+-----------------------------------------------------
+Before running this demo make sure that you have installed the ModeShape EAP kit into an existing JBoss EAP server.
+The simplest way to do this is to follow the instructions provided [here](https://docs.jboss.org/author/display/MODE/Installing+ModeShape+into+AS7)
+
+Start 2 JBoss EAP instances with the configurations provided by this quickstart
+-----------------------------------------------------------------------------------------
+
+1. Copy the `standalone-modeshape-ha-node1.xml` and `standalone-modeshape-ha-node2.xml` configuration files from the root of the quickstart
+into the `JBOSS_HOME/standalone/configuration` folder
+2. Open a command line and navigate to the root of the JBoss server directory.
+3. Start the first server:
+
+        For Linux:   JBOSS_HOME/bin/standalone.sh -c standalone-modeshape-ha-node1.xml
+        For Windows: JBOSS_HOME\bin\standalone.bat -c standalone-modeshape-ha-node1.xml
+4. Start the second server:
+
+        For Linux:   JBOSS_HOME/bin/standalone.sh -c standalone-modeshape-ha-node2.xml
+        For Windows: JBOSS_HOME\bin\standalone.bat -c standalone-modeshape-ha-node2.xml
+
+
+Build and Deploy the Quickstart into each of the running servers
+----------------------------------------------------------------
+_NOTE: The following build command assumes you have configured your Maven user settings. If you have not, you must use the `settings.xml`
+file from the root of this project. See [this ModeShape community article](http://community.jboss.org/wiki/ModeShapeandMaven)
+for help on how to install and configure Maven 3._
+
+1. Make sure you have started the 2 JBoss Server instances as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. Type this command to build and deploy the archive into the first server:
+
+        mvn clean package jboss-as:deploy
+
+4. Type this command to build and deploy the archive into the second server:
+
+        mvn clean package jboss-as:deploy -Djboss-as.port=10000
+
+5. This will deploy `target/modeshape-clustering.war` to each of the running instances of the server.
+
+Accessing the application
+---------------------
+
+The application will be running at the following URLs:
+
+        On the first server: <http://localhost:8080/modeshape-clustering/>
+        On the second server: <http://localhost:8081/modeshape-clustering/>
+
+Open the above URLs in two different browsers (or 2 different browser tabs/windows).
+
+The user is presented with a form where he can input one of the following:
+
+1. Parent Absolute Path - an absolute node path
+2. New Node Name - a simple string which represents the name of new node that can be added
+3. Search for Nodes Like - a simple string which represents the name pattern of nodes that will be searched
+
+based on which one of the following actions can be performed
+
+1. Show children - displays the children of node located at "Parent Absolute Path"
+2. Add Node - add a new child with the given name under the node located at "Parent Absolute Path"
+3. Search - searches for nodes which have in their name the given string pattern
+
+Undeploy the Archive
+--------------------
+
+1. Make sure you have started the 2 JBoss Server instances as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. When you are finished testing, type this command to undeploy the archive from the first server:
+
+        mvn jboss-as:undeploy
+
+4. When you are finished testing, type this command to undeploy the archive from the second server:
+
+        mvn jboss-as:undeploy -Djboss-as.port=10000
+
+Run the Arquillian Tests
+-------------------------
+
+This quickstart provides Arquillian tests. By default, these tests are configured to be skipped as Arquillian tests require the use of a container.
+
+1. Make sure you have started the JBoss Server corresponding to `node1` as described above.
+2. Open a command line and navigate to the root directory of this quickstart.
+3. Type the following command to run the test goal with the following profile activated:
+
+        mvn clean package -Parq-jbossas-remote
+
+The ModeShape project
+---------------------
+ModeShape is an open source implementation of the JCR 2.0 
+([JSR-283](http://www.jcp.org/en/jsr/detail?id=283])) specification and 
+standard API. To your applications, ModeShape looks and behaves like a 
+regular JCR repository. Applications can search, query, navigate, change, 
+version, listen for changes, etc. But ModeShape can store that content 
+in a variety of back-end stores (including relational databases, Infinispan 
+data grids, JBoss Cache, etc.), or it can access and update existing content 
+from *other* kinds of systems (including file systems, SVN repositories, 
+JDBC database metadata, and other JCR repositories). ModeShape's connector 
+architecture means that you can write custom connectors to access any 
+kind of system. And ModeShape can even federate multiple back-end systems 
+into a single, unified virtual repository.
+
+For more information on ModeShape, including getting started guides, 
+reference guides, and downloadable binaries, visit the project's website 
+at [http://www.modeshape.org]() or follow us on our [blog](http://modeshape.wordpress.org) 
+or on [Twitter](http://twitter.com/modeshape). Or hop into our 
+[IRC chat room](http://www.jboss.org/modeshape/chat) and talk our community 
+of contributors and users.
+
+The official Git repository for the project is also on GitHub at 
+[http://github.com/ModeShape/modeshape]().
+
+Need help ?
+-----------
+
+ModeShape is open source software with a dedicated community. If you have 
+any questions or problems, post a question in our 
+[user forum](http://community.jboss.org/en/modeshape) or hop into our 
+[IRC chat room](http://www.jboss.org/modeshape/chat) and talk our 
+community of contributors and users.
---
+++ modeshape-clustering/src/main/java/org/modeshape/quickstart/cdi/ClusteringController.java
@@ -0,0 +1,210 @@
+package org.modeshape.quickstart.cdi;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+import javax.annotation.Resource;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
+import org.apache.log4j.Logger;
+
+/**
+ * A simple JSF controller, that uses a repository which is injected directly,
+ *
+ * @author Horia Chiorean (hchiorea@redhat.com)
+ */
+@Named( "clusteringController" )
+@ApplicationScoped
+public class ClusteringController {
+
+    private static final Logger LOGGER = Logger.getLogger(ClusteringController.class);
+
+    @Resource(mappedName = "java:/jcr/clustered-repo")
+    private transient Repository clusteredRepository;
+
+    private String parentPath = "/";
+    private String newNodeName;
+    private String nodeNamePattern;
+
+    private Set<String> nodes = Collections.emptySet();
+
+    /**
+     * Sets a name for a new node to create.
+     *
+     * @param newNodeName a {@code non-null} string
+     */
+    public void setNewNodeName( String newNodeName ) {
+        this.newNodeName = newNodeName;
+    }
+
+    /**
+     * Returns the name of a new node which can be created.
+     *
+     * @return a {@code non-null} string
+     */
+    public String getNewNodeName() {
+        return newNodeName;
+    }
+
+    /**
+     * Returns the pattern of the name of the node to search after.
+     *
+     * @return a {@code non-null} string
+     */
+    public String getNodeNamePattern() {
+        return nodeNamePattern;
+    }
+
+    /**
+     * Sets the name pattern of the node to search after.
+     *
+     * @param nodeNamePattern a {@code non-null} string
+     */
+    public void setNodeNamePattern( String nodeNamePattern ) {
+        this.nodeNamePattern = nodeNamePattern;
+    }
+
+    /**
+     * Returns the absolute path of a parent node
+     *
+     * @return a {@code non-null} string
+     */
+    public String getParentPath() {
+        return parentPath;
+    }
+
+    /**
+     * Sets the absolute path of a parent node.
+     *
+     * @param parentPath a {@code non-null} string
+     */
+    public void setParentPath( String parentPath ) {
+        this.parentPath = parentPath;
+    }
+
+    /**
+     * Returns the children nodes of the node located at {@link ClusteringController#parentPath}
+     *
+     * @return a Set<String> containing the paths of the children.
+     */
+    public Set<String> getNodes() {
+        return nodes;
+    }
+
+    /**
+     * Searches for nodes that have a given pattern in their [jcr:name].
+     *
+     * @return the view to redirect to.
+     */
+    public String searchForNodes() {
+        nodes = new TreeSet<String>();
+        if (nodeNamePattern == null || nodeNamePattern.trim().length() == 0) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                    "The node name pattern is required"));
+        } else  {
+            Session repositorySession = null;
+            try {
+                repositorySession = newSession();
+                QueryManager queryManager = repositorySession.getWorkspace().getQueryManager();
+                String queryString = "select node.[jcr:name] from [nt:unstructured] as node where node.[jcr:name] like '%" + nodeNamePattern + "%'";
+                LOGGER.info("Executing query:" + queryString);
+                QueryResult queryResult = queryManager.createQuery(queryString, Query.JCR_SQL2).execute();
+                for (NodeIterator nodeIterator = queryResult.getNodes(); nodeIterator.hasNext(); ) {
+                    nodes.add(nodeIterator.nextNode().getPath());
+                }
+            } catch (RepositoryException e) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+            } finally {
+                if (repositorySession != null) {
+                    repositorySession.logout();
+                }
+            }
+        }
+        return "/main.xhtml";
+    }
+
+
+    /**
+     * Loads the children nodes of the node located at {@link ClusteringController#parentPath}
+     *
+     * @return the view to redirect to.
+     */
+    public String loadChildren() {
+        nodes = new TreeSet<String>();
+        if (parentPath == null || parentPath.trim().length() == 0) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                    "The absolute path of the parent node is required"));
+        } else {
+            Session repositorySession = null;
+            try {
+                repositorySession = newSession();
+                Node parentNode = repositorySession.getNode(parentPath);
+                for (NodeIterator nodeIterator = parentNode.getNodes(); nodeIterator.hasNext(); ) {
+                    nodes.add(nodeIterator.nextNode().getPath());
+                }
+            } catch (RepositoryException e) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+            } finally {
+                if (repositorySession != null) {
+                    repositorySession.logout();
+                }
+            }
+        }
+        return "/main.xhtml";
+    }
+
+    /**
+     * Adds a child node at {@link ClusteringController#parentPath} which has the name {@link ClusteringController#newNodeName}
+     *
+     * @return the view to redirect to.
+     */
+    public String addChildNode() {
+        if (newNodeName == null || newNodeName.trim().length() == 0) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("The name of the new node is required"));
+        } else {
+            Session repositorySession = null;
+            try {
+                repositorySession = newSession();
+                Node parentNode = repositorySession.getNode(parentPath);
+                parentNode.addNode(newNodeName);
+                repositorySession.save();
+            } catch (RepositoryException e) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+            } finally {
+                if (repositorySession != null) {
+                    repositorySession.logout();
+                }
+            }
+        }
+        return loadChildren();
+    }
+
+    /**
+     * Returns the name of the repository to which the session is bound.
+     *
+     * @return a non-null string.
+     * @throws RepositoryException if anything unexpected fails
+     */
+    public String getRepositoryName() throws RepositoryException{
+        Session repositorySession = newSession();
+        try {
+            return repositorySession.getRepository().getDescriptor(org.modeshape.jcr.api.Repository.REPOSITORY_NAME);
+        } finally {
+            repositorySession.logout();
+        }
+    }
+
+    private Session newSession() throws RepositoryException {
+        return clusteredRepository.login();
+    }
+}
